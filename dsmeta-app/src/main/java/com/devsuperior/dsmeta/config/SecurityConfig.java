849f0fa96a2612eff62501eadf92b4a73fe7d903
yeti <guizits200@gmail.com>
2023-11-10 23:00:12 -0300

    Axios, useEffect first request

--- dsmeta-app/src/main/java/com/devsuperior/dsmeta/config/SecurityConfig.java
+++ dsmeta-app/src/main/java/com/devsuperior/dsmeta/config/SecurityConfig.java
@@ -10,12 +10,14 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
+@CrossOrigin(origins = "*")
 public class SecurityConfig {
 
 	@Bean
